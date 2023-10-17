4f50a18fa14fa4ed80680c3f6bbf4a0c0af4190d
GianlucaJC <morescogianluca@gmail.com>
2023-10-17 10:13:34 +0200

    Update conn

--- conn.php
+++ conn.php
@@ -1,26 +1,16 @@
 <?php
 
 $servJ="localhost";
-$databaseJ="sql58368_4";
-$utJ="root";
-$passJ="giatongia6971";
+$databaseJ="";
+$utJ="";
+$passJ="";
 
 $servT="localhost";
-$databaseT="LIOFILCHEM";
-$utT="root";
-$passT="giatongia6971";
+$databaseT="";
+$utT="";
+$passT="";
 
-/*
-$servJ='192.168.129.30';
-$databaseJ="Sql58368_4";
-$utJ='sysadmin';
-$passJ='Password01.';
 
-$servT='192.168.129.20';
-$databaseT="LIOFILCHEM";
-$utT='jolly';
-$passT='zxcvbnm';
-*/
 
 $tb_lotti="impegnolotti";
 
